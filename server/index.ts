1ddf26f21bb26127154817187573499919f2ef98
Parshva Runwal <runwal.parshva@hotmail.com>
2023-06-11 03:37:06 +0530

    Set-up a template for swagger documentation

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 import logger from "./lib/logger";
 import authRouter from "./routes/auth";
 import propertiesRouter from "./routes/properties";
@@ -11,6 +13,51 @@ const { PORT = 8000 } = process.env;
 
 const app = express();
 
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerSpecifications = swaggerJsDoc({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Sqrft API",
+      version: "1.0.0",
+      description: "Sqrft API Information",
+      contact: {
+        name: "Sqrft",
+        url: "http://127.0.0.1:8000/api/v1/health",
+        email: "contact.sqrft@gmail.com",
+      },
+    },
+    servers: [{ url: "http://127.0.0.1:8000" }],
+  },
+  apis: ["./routes/*.ts"],
+});
+
 // Register Middlewares
 app.use(express.json());
 
@@ -21,6 +68,13 @@ app.get(`/api/v1/health`, (req, res) => {
 // Register Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/properties", propertiesRouter);
+
+// Swagger
+app.use(
+  "/docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerSpecifications, { explorer: true })
+);
 
 const globalErrorHandler: GlobalErrorHandler = (error, req, res, next) => {
   logger.error(error);
